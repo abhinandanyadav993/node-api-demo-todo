e2f000f73aad05f8c92ed13a508ed1e02647a249
Abhinandan <abhinandan@approutes.com>
2018-05-07 15:17:41 +0530

    Add get todos

--- server/server.js
+++ server/server.js
@@ -24,6 +24,14 @@ app.post('/todos', (req, res) => {
 })
 
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+            res.send({todos});
+    }, (err) =>{
+        res.status(400).send(err);
+    });
+})
+
 const port = process.env.port | 1300;
 app.listen(port ,() =>{
     console.log(`server running on ${port} port`);
